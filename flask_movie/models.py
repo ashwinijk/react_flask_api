a4439945aff9b48d5f2a049089f59cd2e88733fc
ashwini jaya kumar <ashwinijk18@gmail.com>
2020-10-02 12:42:40 +0530

    adding combine flask_react

---
+++ flask_movie/models.py
@@ -0,0 +1,9 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+class Movie(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(50))
+    rating = db.Column(db.Integer)
